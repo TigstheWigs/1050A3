b837990d09562644a6fcc4eae2d971ef738acaad
TigstheWigs <134679791+TigstheWigs@users.noreply.github.com>
2023-05-26 23:08:02 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector('h1');
+
+Fancybox.bind( "[data-fancybox]" );
+
+$( 'button' ).click( filterItems );
+
+function filterItems() {
+    let button = this;
+    let selector = $( button ).data( 'filter' );
+    console.log( selector );
+    $( '#products img' ).not( selector ).hide( 500 );
+    $( selector ).show( 500 );
+
+
+
+}
